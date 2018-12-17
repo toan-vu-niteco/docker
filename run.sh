687988d037a5bf2ed1bc2943f2269a091d986c85
toan-vu-niteco <toan.vu@niteco.se>
2018-12-17 13:12:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,13 +10,13 @@ if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
     touch /etc/phpmyadmin/config.user.inc.php
 fi
 
-mkdir -p /var/nginx/client_body_temp
-chown nobody:nogroup /sessions /var/nginx/client_body_temp
-mkdir -p /var/run/php/
-chown nobody:nogroup /var/run/php/
-touch /var/log/php-fpm.log
-chown nobody:nogroup /var/log/php-fpm.log
+#mkdir -p /var/nginx/client_body_temp
+#chown nobody:nogroup /sessions /var/nginx/client_body_temp
+#mkdir -p /var/run/php/
+#chown nobody:nogroup /var/run/php/
+#touch /var/log/php-fpm.log
+#chown nobody:nogroup /var/log/php-fpm.log
 
-chmod 644 /etc/phpmyadmin/*
+#chmod 644 /etc/phpmyadmin/*
 
 exec "$@"
